66f3a148f4da029da1f4cdb45fa1416118766568
Reakwon Lucciano <62098320+reakwonlucciano@users.noreply.github.com>
2022-12-10 15:21:39 +0000

    Update books.js

--- src/components/books.js
+++ src/components/books.js
@@ -2,10 +2,10 @@ import React from "react";
 import {BookItem} from './bookItem';
 
 export class Books extends React.Component{
-    render(){
+    render(){ //Take collection of books, split into individual books and return bookItem
         return this.props.books.map(
             (book)=>{
-                return <BookItem book={book} key={book._id}></BookItem>
+                return <BookItem book={book} key={book._id} Reload = {this.props.Reload}></BookItem>
             }
         );
     }
