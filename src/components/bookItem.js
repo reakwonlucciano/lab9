d400ba242880d1c6a18e677112349892b0661090
Reakwon Lucciano <62098320+reakwonlucciano@users.noreply.github.com>
2022-12-10 15:12:55 +0000

    Update bookItem.js

--- src/components/bookItem.js
+++ src/components/bookItem.js
@@ -1,7 +1,25 @@
 import React from "react";
 import Card from 'react-bootstrap/Card';
 import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button"; //Imports bootstrap button
+import axios from "axios"; //Imports a promise
+
 export class BookItem extends React.Component {
+
+    //Constructor for the BookItem class
+    constructor() {
+        super(); //Calls parents constructor
+        this.DeleteBook = this.DeleteBook.bind(this); //Creates a binding for the event method delete
+    }
+
+    //This is a delete method that acts as a event
+    DeleteBook(e) {
+        e.preventDefault();
+        axios.delete('http://localhost:4000/api/book/' + this.props.book._id) // Makes request to server and waits for database to delete book with given id
+            .then((res) => { this.props.Reload(); }) //If successful, call Reload which refreshes data on the page
+            .catch();
+    }
+
     render() {
         return (
             <div>
@@ -9,6 +27,7 @@ export class BookItem extends React.Component {
         <img src={this.props.book.thumbnailUrl}></img>
                 <h6>{this.props.book.authors[0]}</h6> */}
 
+                {/*Creates a new Card that contains details about the particular book*/}
                 <Card>
                     <Card.Header>{this.props.book.title}</Card.Header>
                     <Card.Body>
@@ -19,7 +38,8 @@ export class BookItem extends React.Component {
                             </footer>
                         </blockquote>
                     </Card.Body>
-                <Link to={'/edit/'+this.props.book._id} className="btn btn-primary">Edit</Link>
+                    <Link to={'/edit/' + this.props.book._id} className="btn btn-primary">Edit</Link> {/**Creates a new Edit button Link that sends user to edit page upon clicking it*/}
+                    <Button variant="danger" onClick={this.DeleteBook}>Delete</Button> {/*Creates a delete button, when its clicked, what event should be called?*/}
                 </Card>
             </div>
         );
