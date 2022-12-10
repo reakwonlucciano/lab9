ab918f95bc150a1768ffb484ea6e9e867a0886eb
Reakwon Lucciano <62098320+reakwonlucciano@users.noreply.github.com>
2022-12-10 15:22:53 +0000

    Update read.js

--- src/components/read.js
+++ src/components/read.js
@@ -4,15 +4,26 @@ import axios from "axios";
 
 export class Read extends React.Component{
     
+//Creates constructor for class
+    constructor(){
+        super(); //Call parents constructor
+        this.Reload = this.Reload.bind(this); //Creates binding for Reload
+    }
+    //Go off to server and pull new list of books
+    //Refreshes data of books - this acts as a event
+    Reload() {
+        this.componentDidMount();
+    }
 
+    //Acts as a lifecycle hook for the start of a component
     componentDidMount() {
         axios.get('http://localhost:4000/api/books')
-        .then((response)=>{
-            this.setState({books:response.data})
-        })
-        .catch((error)=>{
-            console.log(error);
-        })
+            .then((response) => {
+                this.setState({ books: response.data })
+            })
+            .catch((error) => {
+                console.log(error);
+            })
     }
 
     state = {
@@ -23,7 +34,7 @@ export class Read extends React.Component{
         return(
             <div>
                 <h3>Hello from my Read component!</h3>
-                <Books books={this.state.books}></Books>
+               <Books books={this.state.books} Reload={this.Reload}></Books>
             </div>
         );
     }
